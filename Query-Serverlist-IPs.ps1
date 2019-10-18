79ba4f94c882254c3e38f197c7068a038705569c
tmrayer <50215787+tmrayer@users.noreply.github.com>
2019-10-18 10:42:58 -0500

    Add files via upload

---
+++ Query-Serverlist-IPs.ps1
@@ -0,0 +1,8 @@
+﻿## Configure a list of server in the path
+## This script will ping each entry in comma delimited format
+$servers = Get-Content c:\scripts\serverList.txt
+$ping = New-Object System.Net.NetworkInformation.Ping
+foreach($s in $servers)
+{
+    $("$s,$($ping.Send($s).Address)")
+}
